74c81467a870a4d8ae838c6ba4c49b21c8b1d8d3
Jeremiah Ginder <ginderjeremiah@gmail.com>
2026-06-12 21:49:03 -0500

    add length min/max for username and password fields on login requests

--- Game.Api/Models/Player/LoginCredentials.cs
+++ Game.Api/Models/Player/LoginCredentials.cs
@@ -1,8 +1,13 @@
-﻿namespace Game.Api.Models.Player
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Game.Api.Models.Player
 {
     public class LoginCredentials : IModel
     {
+        [Length(1, 25)]
         public required string Username { get; set; }
+
+        [Length(1, 256)]
         public required string Password { get; set; }
     }
 }
